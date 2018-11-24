ff50f0b7571e496faf1f4ab995fd6ee55c5d5790
thangl3 <lethethangdn@gmail.com>
2018-11-24 15:24:33 +0700

    Develop response object

--- helper/route/http/Response.php
+++ helper/route/http/Response.php
@@ -1,7 +1,39 @@
 <?php
 namespace Helper\Route\Http;
 
+use Exception;
+
 class Response implements ResponseInterface
 {
+    private $output;
 
+    public function write($output) : ResponseInterface
+    {
+        try {
+            ob_start();
+            if (is_callable($output)) {
+                echo $output();
+            } else {
+                echo $output;
+            }
+        } catch (Exception $ex) {
+            // keep slient this exception because this is the last handle error
+            // throw $ex;
+        } finally {
+            echo ob_get_clean();
+            ob_end_flush();
+        }
+
+        return $this;
+    }
+
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+
+    public function getOutput()
+    {
+        return $this->output;
+    }
 }
--- helper/route/http/ResponseInterface.php
+++ helper/route/http/ResponseInterface.php
@@ -3,5 +3,5 @@ namespace Helper\Route\Http;
 
 interface ResponseInterface
 {
-    
+    public function write($output) : self;
 }
