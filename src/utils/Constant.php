f9742bdbf32ed1176c6889b93f100eb258e47133
thangl3 <lethethangdn@gmail.com>
2018-11-24 20:18:39 +0700

    Add constant file

---
+++ src/utils/Constant.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Utils;
+
+class Constant
+{
+    const CREATE_SUCCESS = 'Create a work success!';
+    const CREATE_FAIL = 'Create a work fail!';
+    const UPDATE_SUCCESS = 'Update the work success!';
+    const UPDATE_FAIL = 'Update the work fail!';
+    const DELETE_SUCCESS = 'Delete a work success!';
+    const DELETE_FAIL = 'Delete a work fail!';
+    const STATUS_NOT_VALID = 'Status not valid, please try again!';
+    const STARTDATE_NOT_VALID = 'Start date not valid, please try again!';
+    const ENDDATE_NOT_VALID = 'End date not valid, please try again!';
+}
